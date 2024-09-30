eeb1bad50f3afc342030d863cd3a8d64c1f579ec
wipeseals <wipeseals@gmail.com>
2024-09-30 22:17:18 +0900

    feat: Program雛形作成

---
+++ Packages/com.wipeseals.udon-knucklebones/Runtime/Udon/UdonKnucklebones.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class UdonKnucklebones : UdonSharpBehaviour
+{
+    [Header("Udon Knucklebones")]
+    [SerializeField, Tooltip("Debug Enabled")]
+    public bool isDebug = true;
+
+    [SerializeField, Tooltip("The dice object to roll")]
+    public GameObject dice = null;
+
+    /// <summary>
+    /// Prints a message to the console
+    /// </summary>
+    /// <param name="msg"></param>
+    void Log(string msg)
+    {
+        if (isDebug)
+        {
+            Debug.Log($"[UdonKnucklebones] {msg}");
+        }
+    }
+
+    void Start()
+    {
+        Log("Udon Knucklebones is ready!");
+    }
+}
