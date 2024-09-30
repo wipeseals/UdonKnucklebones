771abe8a186be72c449821aa6c84b94e7f3d46d4
wipeseals <wipeseals@gmail.com>
2024-09-30 23:09:36 +0900

    feat(wip): Field追加

--- Packages/com.wipeseals.udon-knucklebones/Runtime/Udon/UdonKnucklebones.cs
+++ Packages/com.wipeseals.udon-knucklebones/Runtime/Udon/UdonKnucklebones.cs
@@ -1,18 +1,87 @@
 ﻿
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
-
 [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 public class UdonKnucklebones : UdonSharpBehaviour
 {
+    #region Constants
+    /// <summary>
+    /// The number of columns in the knucklebones game
+    /// </summary>
+    public readonly int NUM_COLUMNS = 3;
+    /// <summary>
+    /// The number of rows in the knucklebones game
+    /// </summary>
+    public readonly int NUM_ROWS = 3;
+    #endregion
+
+    #region Properties
+
     [Header("Udon Knucklebones")]
     [SerializeField, Tooltip("Debug Enabled")]
-    public bool isDebug = true;
+    public bool IsDebug = true;
 
     [SerializeField, Tooltip("The dice object to roll")]
-    public GameObject dice = null;
+    public GameObject Dice = null;
+
+
+    [SerializeField, Tooltip("The dice array to visualize for player 1 column 1")]
+    public GameObject[] Player1Col1DiceArray = null;
+
+    [SerializeField, Tooltip("The dice array to visualize for player 1 column 2")]
+    public GameObject[] Player1Col2DiceArray = null;
+
+    [SerializeField, Tooltip("The dice array to visualize for player 1 column 3")]
+    public GameObject[] Player1Col3DiceArray = null;
+
+    [SerializeField, Tooltip("The dice array to visualize for player 2 column 1")]
+    public GameObject[] Player2Col1DiceArray = null;
+
+    [SerializeField, Tooltip("The dice array to visualize for player 2 column 2")]
+    public GameObject[] Player2Col2DiceArray = null;
+
+    [SerializeField, Tooltip("The dice array to visualize for player 2 column 3")]
+    public GameObject[] Player2Col3DiceArray = null;
+
+    [SerializeField, Tooltip("Score text for player 1")]
+    public Text[] Player1ColumnScoreTexts = null;
+
+    [SerializeField, Tooltip("Score text for player 2")]
+    public Text[] Player2ColumnScoreTexts = null;
+
+    [SerializeField, Tooltip("Main score text for player 1")]
+    public Text Player1MainScoreText = null;
+
+    [SerializeField, Tooltip("Main score text for player 2")]
+    public Text Player2MainScoreText = null;
+
+    [SerializeField, Tooltip("The text to display the current turn")]
+    public Text TurnText = null;
+
+    [SerializeField, Tooltip("The text to display the current system message")]
+    public Text SystemText = null;
+
+    #endregion
+
+    public bool IsSetupComplete()
+    {
+        return Dice != null &&
+            Player1Col1DiceArray != null &&
+            Player1Col2DiceArray != null &&
+            Player1Col3DiceArray != null &&
+            Player2Col1DiceArray != null &&
+            Player2Col2DiceArray != null &&
+            Player2Col3DiceArray != null &&
+            Player1ColumnScoreTexts != null &&
+            Player2ColumnScoreTexts != null &&
+            Player1MainScoreText != null &&
+            Player2MainScoreText != null &&
+            TurnText != null &&
+            SystemText != null;
+    }
 
     /// <summary>
     /// Prints a message to the console
@@ -20,7 +89,7 @@ public class UdonKnucklebones : UdonSharpBehaviour
     /// <param name="msg"></param>
     void Log(string msg)
     {
-        if (isDebug)
+        if (IsDebug)
         {
             Debug.Log($"[UdonKnucklebones] {msg}");
         }
@@ -28,6 +97,11 @@ public class UdonKnucklebones : UdonSharpBehaviour
 
     void Start()
     {
+        if (!IsSetupComplete())
+        {
+            Debug.LogError("Udon Knucklebones is not setup correctly!");
+            return;
+        }
         Log("Udon Knucklebones is ready!");
     }
 }
